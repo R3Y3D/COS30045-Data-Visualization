5782756410e78fdf686d62349dcbcf99e1f1da04
Ibrahim Mahbub <J22036979@student.newinti.edu.my>
2026-09-16 16:18:41 +0800

    add: step 1 completed for d3 charts and activated

---
+++ Exercise 4/Exercise 4.3/assets/js/d3_main.js
@@ -0,0 +1,14 @@
+/**
+ * COS30045 - Data Visualisation
+ * Exercise 4.2: D3 Implementation
+ */
+
+// Ensure D3 library loaded properly
+console.log("D3 library version:", d3.version);
+
+// Document ready wrapper for D3 logic
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("D3 script initialized and ready.");
+  
+  // Your Step 2+ D3 selections and SVG manipulations will go here
+});
